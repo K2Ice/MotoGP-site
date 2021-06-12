48f2e7eb7eaf24fa83db7eb1e20ca048a87e6afd
Konrad Korzeniewski <k.korzeniewski94@gmail.com>
2021-06-12 21:38:56 +0200

    Add jquery to navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
+import $ from 'jquery'
 
 import Footer from "./Footer"
 import Header from "./Header"
@@ -8,19 +9,60 @@ import Riders from "./Riders"
 import Navigation from "./Navigation"
 
 import "../styles/App.css"
+import { element } from "prop-types"
 
 class App extends React.Component {
   state = {
     chosenTeam: "all",
+    mainStyle: false,
   }
-  // componentDidMount() {
-  //   fetch("./data.json")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(typeof data)
-  //       return this.setState({ data })
-  //     })
-  // }
+  componentDidMount() {
+    const header = $(".headPic")
+    const nav = $(".mainNav")
+    let headerScrolled = true
+    
+    $(document).on("scroll", function () {
+      const skroll = $(document).scrollTop()
+      let activeElement = Array.from($(".mainNav a")).filter(element=>$(element).hasClass('active'))
+    
+      if (skroll >= header.height() && headerScrolled) {
+        nav.css({
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          zIndex: "10",
+          transform: "translateX(-50%)",
+          borderRadius: "20px",
+          border: "1px solid black",
+          overflow: "hidden",
+        })
+       
+        $(".mainNav a").css({ padding: "10px 20px",backgroundColor: "rgba(0, 0, 0, 1)", })
+
+        $(activeElement).css({backgroundColor: 'rgba(252, 248, 0, 0.671)',
+        fontWeight: 700,})
+        headerScrolled = !headerScrolled
+      } else if (skroll <= header.height() && !headerScrolled) {
+        nav.css({
+          position: "static",
+          top: "auto",
+          left: "auto",
+          transform: "none", 
+          border: "1px solid black",
+          borderRadius: "0px",
+        })
+          $(".mainNav a").css({
+          padding: "20px 30px",
+          backgroundColor: "rgba(0, 0, 0, 1)",
+        })
+        headerScrolled = !headerScrolled
+        $(activeElement).css({backgroundColor: 'rgba(252, 248, 0, 0.671)',
+        fontWeight: 700,})
+      }
+      
+    })
+    
+  }
 
   handleTeamChoice = (e) => {
     const team = e.target.value
