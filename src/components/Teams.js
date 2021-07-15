6bf6fb9e40d9713ee4b49d0bf1a575017492143d
Konrad Korzeniewski <k.korzeniewski94@gmail.com>
2021-07-16 00:32:55 +0200

    Add teams logos and modify teams component

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -3,18 +3,37 @@ import React from "react"
 import "../styles/Teams.css"
 
 const Teams = ({ data }) => {
-  let teams = data.riders.map((rider) => rider.team)
-  const teamsTab = []
-  teams.forEach((team) => {
-    if (!teamsTab.includes(team)) teamsTab.push(team)
+  let teams = data.riders.map((rider) => ({
+    team: rider.team,
+    moto: rider.motorcycle,
+  }))
+
+  let teamTab = []
+  teams.forEach((element) => {
+    if (teamTab.length === 0) return teamTab.push(element)
+    let isAdded = false
+    let index = 0
+    while (!isAdded) {
+      if (index === teamTab.length) {
+        teamTab.push(element)
+        isAdded = true
+      }
+      if (element.team === teamTab[index].team) isAdded = true
+      index++
+    }
   })
 
-  teams = teamsTab.map((team) => <p>{team}</p>)
-  return (
-    <div className="teams">
-      <p>{teams}</p>
+  teamTab = teamTab.map((teamItem) => (
+    <div className="team-element">
+      <img
+        src={`./pic/logos/${teamItem.moto.toLowerCase()}-vector-logo.png`}
+        alt={`${teamItem.moto}`}
+      />
+      <p>{teamItem.team}</p>
     </div>
-  )
+  ))
+
+  return <div className="teams">{teamTab}</div>
 }
 
 export default Teams
